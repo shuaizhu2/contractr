a9a6fe930dda358839c032ac0c980ad2d36e28e7
shuaizhu1 <szhu12@ucsc.edu>
2016-01-30 12:04:18 -0800

    First Commit

---
+++ hackucsc.js
@@ -0,0 +1,146 @@
+Jobs = new Mongo.Collection('jobs');
+Users = new Mongo.Collection('users');
+
+if (Meteor.isClient) {
+  var MAP_ZOOM = 15;
+
+  Meteor.startup(function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position);
+        Session.set('lat', position.coords.latitude);
+        Session.set('lon', position.coords.longitude);
+    });
+
+    GoogleMaps.load();
+  });
+
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.home.helpers({
+    jobs: function(){
+      var query = Session.get('lF');
+      console.log(query);
+      //var selector;
+      if (query){
+        var re = new RegExp(query, 'i');
+        console.log(re);
+        //var re = "/" + query + "/";
+        //selector.category = re;
+        //db.users.findOne({"username" : {$regex : ".*son.*"}});
+        var j = Jobs.find({'category': {$regex : re}});
+      } else {
+        console.log("miss");
+        var j = Jobs.find({}).fetch();
+      }
+      
+      for (var i=0; i<j.length; i++) {
+        j[i].id = "Job" + i;
+      }
+      return j;
+    }
+  });
+
+  Template.home.events({
+    "click #createJobs": function(){
+      Meteor.call("addJob", "Plumbing");
+      Meteor.call("addJob", "Painting");
+      Meteor.call("addJob", "Moving");
+      Meteor.call("addJob", "Cleaning");
+    },
+    "submit #lookingFor": function(e){
+      e.preventDefault();
+      console.log("submit");
+    },
+    "keyup #lookingFor": function(){
+      console.log($('#lookingFor').val());
+      return Session.set('lF', $('#lookingFor').val());
+    }
+  });
+
+  Template.map.helpers({  
+    geolocationError: function() {
+      var error = Geolocation.error();
+      return error && error.message;
+    },
+    mapOptions: function() {
+      var latLng = Geolocation.latLng();
+      // Initialize the map once we have the latLng.
+      if (GoogleMaps.loaded() && latLng) {
+        return {
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: MAP_ZOOM
+        };
+      }
+    }
+  });
+
+  Template.map.onCreated(function() {  
+    var self = this;
+
+    GoogleMaps.ready('map', function(map) {
+      var marker;
+
+      // Create and move the marker when latLng changes.
+      self.autorun(function() {
+        var latLng = Geolocation.latLng();
+        if (! latLng)
+          return;
+
+        // If the marker doesn't yet exist, create it.
+        if (! marker) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latLng.lat, latLng.lng),
+            map: map.instance
+          });
+        }
+        // The marker already exists, so we'll just change its position.
+        else {
+          marker.setPosition(latLng);
+        }
+
+        // Center and zoom the map view onto the current position.
+        map.instance.setCenter(marker.getPosition());
+        map.instance.setZoom(MAP_ZOOM);
+      });
+    });
+  });
+}
+
+Meteor.methods({
+  //Individual Jobs, Times, description, contractor, contractee
+  addJob: function(category) {
+    Jobs.insert({
+      category: category,
+      called: 0,
+      contractors: [],
+      //query by tasks, avaliability and distance
+      // tasks
+    });
+  }//,
+  /*AddUser: function(name, type) {
+    //i.e Shuai Zhu, Contractor
+    //Simulated with paypal, etc.
+    Users.insert({
+      name: name,
+      type: type,
+      location: 
+    });
+  },*/
+  //AddContractor: function()
+});
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    Meteor.publish("jobs", function () {
+      return Jobs.find({
+      });
+    });
+
+    Meteor.publish("users", function () {
+      return Users.find({
+      });
+    });
+    // code to run on server at startup
+  });
+}
